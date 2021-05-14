a0592750e03cc3d9ec757dfed32a8b4e8bf323d0
Igor <igor.nikiforov@domru.ru>
2021-05-14 10:37:58 +0500

    Added start project

---
+++ BestMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BestMusic
+//
+//  Created by Игорь Никифоров on 14.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
