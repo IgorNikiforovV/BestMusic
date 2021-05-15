2436e76ecfdb17b85b6c919f4c241e0989de26c0
Igor <igor.nikiforov@domru.ru>
2021-05-15 17:44:36 +0500

    Added UITabBarController, SearchViewController
    1. Added UITabBarController, SearchViewController
    2. Added tabBar icons

---
+++ BestMusic/Controllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  BestMusic
+//
+//  Created by Игорь Никифоров on 15.05.2021.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+}
