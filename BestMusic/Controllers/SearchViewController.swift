6aa4bda999b2bfabca4f910815d7e1cdd1b4fa97
Igor <igor.nikiforov@domru.ru>
2021-05-16 17:42:23 +0500

    Added SearchBar and tests table view cells

--- BestMusic/Controllers/SearchViewController.swift
+++ BestMusic/Controllers/SearchViewController.swift
@@ -7,12 +7,74 @@
 
 import UIKit
 
+struct TrackModel {
+    let trackName: String
+    let artistName: String
+}
+
 class SearchViewController: UITableViewController {
+
+    private let searchController = UISearchController(searchResultsController: nil)
+
+    private let tracks = [TrackModel(trackName: "Save Your Tears", artistName: "The Weeknd"),
+                          TrackModel(trackName: "Diamonds", artistName: "Sam Smith"),
+                          TrackModel(trackName: "Regardless", artistName: "RAYE&Rudimental")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
+        configureSearchBar()
+        configureTableView()
+
+    }
+
+}
+
+// MARK: UITableViewDataSource -
+
+extension SearchViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tracks.count
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.textLabel?.numberOfLines = 2
+        cell.textLabel?.text = tracks[indexPath.item].artistName
+        let track = tracks[indexPath.item]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.imageView?.image = UIImage(named: "trackImage")
+        return cell
+    }
+
+}
+
+// MARK: UISearchBarDelegate -
+
+extension SearchViewController: UISearchBarDelegate {
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+
+}
+
+// MARK: Private methods -
+
+private extension SearchViewController {
+
+    func configureTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+    }
+
+    func configureSearchBar() {
+        searchController.searchBar.delegate = self
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
     }
 
 }
